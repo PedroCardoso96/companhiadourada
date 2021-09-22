ca043fbf4f81c9d630f2172406bc7a38e8bb070f
PedroCardoso96 <pedrohfsc96@gmail.com>
2021-09-22 19:16:54 -0300

    to-top e button

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
+// Animation header
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
-
 
 $(window).ready(function(){
     $(".mostrar").hide();
@@ -11,3 +12,39 @@ $(window).ready(function(){
         $(this).next(".mostrar").slideToggle(600);
 });
 });
+
+
+
+// Button To-top
+
+const backToTopButton = document.querySelector('.to-top')
+
+function backToTop(){
+
+    if(window.scrollY >= 400){
+        backToTopButton.classList.add('show')
+    } else{
+        backToTopButton.classList.remove('show')
+    }
+
+}
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 400) {
+            $('.to-top').add('show');
+        } else {
+            $('.to-top').add('show');
+        }
+    });
+
+    $('.to-top').click(function(){
+        $('html, body').animate({scrollTop : 0}, 400);
+        return false;
+    });
+});
+
+
+window.addEventListener('scroll', function(){
+    backToTop()
+})
