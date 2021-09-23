60f6ccf7ce48a2cf3730cd635504d25c8a65d112
PedroCardoso96 <pedrohfsc96@gmail.com>
2021-09-22 23:02:59 -0300

    update

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
     });
 
     $('.to-top').click(function(){
-        $('html, body').animate({scrollTop : 0}, 400);
+        $('html, body').animate({scrollTop : 0}, 0);
         return false;
     });
 });
